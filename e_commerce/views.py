37717c698b549ddbdba6d826bdd7c15d8b89271d
sekhmet <sekhmet@sekhmet>
2022-09-01 23:19:32 -0300

    added new path on views.py and urls.py

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,4 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from matplotlib.style import context
+
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Pagina principal"
+        "content": "Bem-Vindo a página principal"
+    }
+    return render(request, "home_page.html", context)
+
+
+def about_page(request):
+    context = {
+        "title": "Pagina sobre"
+        "content": "Bem-Vindo a página sobre"
+    }
+    return render(request, "about_page.html", context)
+
+
+def contact_page(request):
+    context = {
+        "title": "Pagina de Contato"
+        "content": "Bem-Vindo a pagina de contato"
+    }
+    return render(request, "contact_page.html", context)
