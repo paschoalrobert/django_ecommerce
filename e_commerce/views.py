fc2f4caec419ba228b0c661d6e7f3dcf89acb143
sekhmet <sekhmet@sekhmet>
2022-09-08 00:30:51 -0300

    added forms on contact/view.html and return print requests on view.py

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
 def home_page(request):
     context = {
         "title": "Pagina principal",
@@ -19,5 +20,9 @@ def contact_page(request):
     context = {
         "title": "Pagina de Contato",
         "content": "Bem-Vindo a pagina de contato"
+
     }
+    if request.method == "POST":
+        print(request.POST)
+        
     return render(request, "contact/view.html", context)
