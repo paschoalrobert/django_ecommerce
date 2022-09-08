9ad237f3dd2e3b7abe311fc70ed28aac2e1abee5
sekhmet <sekhmet@sekhmet>
2022-09-07 23:00:38 -0300

    added new code source on about/view.html and contact/view.html

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,26 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from matplotlib.style import context
-
 def home_page(request):
     context = {
-        "title": "Pagina principal"
+        "title": "Pagina principal",
         "content": "Bem-Vindo a página principal"
     }
     return render(request, "home_page.html", context)
 
-
 def about_page(request):
     context = {
-        "title": "Pagina sobre"
+        "title": "Pagina sobre",
         "content": "Bem-Vindo a página sobre"
     }
-    return render(request, "about_page.html", context)
+    return render(request, "about/view.html", context)
 
 
 def contact_page(request):
     context = {
-        "title": "Pagina de Contato"
+        "title": "Pagina de Contato",
         "content": "Bem-Vindo a pagina de contato"
     }
-    return render(request, "contact_page.html", context)
+    return render(request, "contact/view.html", context)
